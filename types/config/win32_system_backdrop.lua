042d3216e13f6d1cc5958007ea9cf29783b0265d
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-10-18 17:10:36 +0200

    `win32_system_backdrop`

---
+++ types/config/win32_system_backdrop.lua
@@ -0,0 +1,8 @@
+---@meta
+
+---@alias SystemBackdrop
+---| "Auto" the system chooses. In practice, this is the same as "Disable". This is the default value.
+---| "Disable" disable backdrop effects.
+---| "Acrylic" enable the Acrylic blur-behind-window effect. Available on Windows 10 and 11.
+---| "Mica" enable the Mica effect, available on Windows 11 build 22621 and later.
+---| "Tabbed" enable the Tabbed effect, available on Windows 11 build 22621 and later.
