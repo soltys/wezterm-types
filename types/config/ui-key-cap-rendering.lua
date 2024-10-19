56ad9ceb40ec95522737e5e70285567def57345e
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-10-19 11:24:59 +0200

    `ui_key_cap_rendering`

---
+++ types/config/ui-key-cap-rendering.lua
@@ -0,0 +1,8 @@
+---@meta
+
+---@alias UIKeyCapRendering
+---| "UnixLong" - `Super`, `Meta`, `Ctrl`, `Shift`
+---| "Emacs" - `Super`, `M`, `C`, `S`
+---| "AppleSymbols" - Use macOS style symbols for Command, Option and so on.
+---| "WindowsLong" - `Win`, `Alt`, `Ctrl`, `Shift`
+---| "WindowsSymbols" - Like `WindowsLong` but using a logo for the Win key.
