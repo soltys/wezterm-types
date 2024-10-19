a63bc68371560f079dd7412e6549414e32e90445
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-10-19 11:18:01 +0200

    `audible_bell`

---
+++ types/config/audible-bell.lua
@@ -0,0 +1,5 @@
+---@meta
+
+---@alias AudibleBell
+---| "SystemBeep" Perform the system beep or alert sound. This is the default. On Wayland systems, which have no system beep function, it does not produce a sound.
+---| "Disabled" Don't make a sound
